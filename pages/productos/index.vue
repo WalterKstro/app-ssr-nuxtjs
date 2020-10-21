3771f372dabfcd56283f99b0361a7da02a731027
Walter Kastro <walterkstro@gmail.com>
2020-10-20 20:17:02 -0600

    Page product completed

--- pages/productos/index.vue
+++ pages/productos/index.vue
@@ -7,19 +7,62 @@
         </b-col>
       </b-row>
     </b-container>
+    <b-container>
+      <b-row>
+        <b-col cols="12" class="grid__card my-md-5">
+          <b-card
+            v-for="product in products"
+            :key="product.id"
+            :title="product.nombre"
+            :img-src="product.imagen"
+            img-alt="Foto del servicio"
+            img-top
+          >
+            <b-card-text>
+              {{ product.descripcion }}
+            </b-card-text>
+            <template v-slot:footer>
+              <strong class="text-right d-block">{{ product.precio | price }}</strong>
+            </template>
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
   </section>
 </template>
 
 <script>
 import BannerProducts from '@/components/BannerProducts'
+import { mapActions, mapState } from 'vuex'
 export default {
   name: 'Productos',
   components: {
     BannerProducts
+  },
+  filters: {
+    price (value) {
+      if (isNaN(value)) {
+        return 'Precio variable'
+      }
+      return `Q ${value}`
+    }
+  },
+  computed: {
+    ...mapState(['products'])
+  },
+  mounted () {
+    this.selectDocuments({ payload: 'productos' })
+  },
+  methods: {
+    ...mapActions(['selectDocuments'])
   }
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.grid__card{
+  display: grid;
+  grid-template-columns: repeat(auto-fill,minmax(20rem,1fr));
+  gap: 2rem;
+}
 </style>
